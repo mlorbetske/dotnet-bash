fd26a4131fbd16fcb93bda39a5cda888a4eb3e36
mlorbetske <mike.lorbetske@n3-p.com>
2016-06-16 20:43:29 -0700

    Update to RTM packages

--- configure.sh
+++ configure.sh
@@ -2,5 +2,6 @@
 sudo cp dotnet.sh /etc/bash_completion.d/dotnet.sh -f
 . /etc/bash_completion.d/dotnet.sh
 source /etc/bash_completion.d/dotnet.sh
-dotnet build -r ubuntu.14.04-x64 -c Release
+dotnet restore --ignore-failed-sources
+dotnet build -c Release -r ubuntu.14.04-x64
 sudo cp bin/Release/netcoreapp1.0/ubuntu.14.04-x64/* /usr/local/bin/
